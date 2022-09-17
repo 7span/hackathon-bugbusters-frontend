2e5ba8ba6c4c95663a845857bf60a7ffde8a561a
Bhavin <bhavin@7span.com>
2022-09-17 13:22:36 +0530

    Default Layout Setup :white_check_mark:

---
+++ components/TheHeader.vue
@@ -0,0 +1,46 @@
+<template>
+  <header class="py-5">
+    <div class="container">
+      <div class="flex justify-between items-center">
+        <nuxt-link to="/" class="text-4xl">
+          <span class="font-thin">Tiny</span>
+          <span class="font-extrabold text-primary-500">Miny</span>
+        </nuxt-link>
+
+        <nav>
+          <nuxt-link
+            v-for="(nav, index) in navs"
+            :key="index"
+            :to="nav.to"
+            class="nav-link hover:text-primary-500 text-base"
+            :class="index !== 0 ? 'ml-3' : ''"
+          >
+            {{ nav.name }}
+          </nuxt-link>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      navs: [
+        {
+          name: 'Home',
+          to: '#'
+        },
+        {
+          name: 'About',
+          to: '#'
+        },
+        {
+          name: 'Contact',
+          to: '#'
+        }
+      ]
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,5 @@
 <template>
   <div>
-    <h1>Hello TinyMiny</h1>
+    <h1>Hello World</h1>
   </div>
 </template>
-
-<script>
-export default {
-  name: 'IndexPage'
-}
-</script>
