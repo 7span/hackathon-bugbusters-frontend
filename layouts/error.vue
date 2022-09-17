6f449603eab7da757d61a1d2a05bb00e9d77227e
jay-p-b-7span <jay.p.b@7span.com>
2022-09-18 04:33:58 +0530

    Testing...

---
+++ layouts/error.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>PAGE NOT FOUND PAGE</div>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log("insisde created layouts");
+    if (process.client) this.redirectShortUrl();
+  },
+  methods: {
+    redirectShortUrl() {
+      const shortUrl = window.location.pathname.split("/").pop();
+
+      this.$axios
+        .post("fetch-main-url", {
+          short_url: shortUrl,
+        })
+        .then((response) => {
+          window.location.href = response.data.data.main_url;
+        });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
