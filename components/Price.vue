0c0705a57627e64ba809dcf6e3f2aea8b1cab856
jay-p-b-7span <jay.p.b@7span.com>
2022-09-18 09:06:27 +0530

    Added Link in Get started button :tada:

--- components/Price.vue
+++ components/Price.vue
@@ -5,14 +5,7 @@
       <div class="text-center mt-4 mb-10 sm:mb-20">
         <h2 class="text-4xl text-dark-500 font-semibold lg:text-6xl">
           <span
-            class="
-              text-xl
-              sm:text-3xl
-              italic
-              font-medium
-              text-dark-500
-              font-extrabold
-            "
+            class="text-xl sm:text-3xl italic font-medium text-dark-500 font-extrabold"
             >plan &</span
           >
           <span class="text-primary-500">pricing</span>
@@ -23,15 +16,7 @@
         <label for="">Select Plan:</label>
         <select
           v-model="selectedPlan"
-          class="
-            w-full
-            p-2
-            border border-primary-500
-            bg-primary-500
-            text-white
-            rounded
-            focus:outline-none
-          "
+          class="w-full p-2 border border-primary-500 bg-primary-500 text-white rounded focus:outline-none"
         >
           <option
             v-for="(item, index) in options"
@@ -87,8 +72,7 @@
       <ButtonPrimary
         type="submit"
         label="Get Started"
-        :disabled="isDisabled"
-        :loader="isLoading"
+        @click.native="$router.push({ name: 'signup' })"
         class="block text-center font-semibold mr-auto ml-auto w-72 mt-10 p-3"
       />
     </div>
@@ -125,26 +109,26 @@ export default {
     "Can see the counts per link",
     "Can see the statistics",
   ],
-  data(){
-    return{
-      isDisabled:false,
-      isLoading:false,
-      selectedPlan:'tiny',
-      options:[
+  data() {
+    return {
+      isDisabled: false,
+      isLoading: false,
+      selectedPlan: "tiny",
+      options: [
         {
-          label:'Tiny',
-          value:'tiny'
+          label: "Tiny",
+          value: "tiny",
         },
         {
-          label:'Small',
-          value:'small'
+          label: "Small",
+          value: "small",
         },
         {
-          label:'Large',
-          value:'large'
-        }
-      ]
-    }
-  }
+          label: "Large",
+          value: "large",
+        },
+      ],
+    };
+  },
 };
 </script>
